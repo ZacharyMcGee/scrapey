ffca87a77ce77c059d1f4f7f45e3352458dec650
Zachary McGee <zachary.mcgee@siu.edu>
2018-11-07 19:06:14 -0600

    Update js/load_page.js

--- js/load_page.js
+++ js/load_page.js
@@ -1,14 +1,17 @@
-  function refreshData(){
-    var display = document.getElementById("site-view");
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("GET", "../php/hello.php");
-    xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xmlhttp.send();
-    xmlhttp.onreadystatechange = function() {
-      if (this.readyState === 4 && this.status === 200) {
-        display.innerHTML = this.responseText;
-      } else {
-        display.innerHTML = "Loading...";
-      };
-    }
+function refreshData(){
+  var site = document.getElementById("input-site").value;
+  console.log(site);
+  var display = document.getElementById("site-view");
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("GET", "php/load_page.php?site=" + site, true);
+  xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xmlhttp.send();
+  xmlhttp.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status === 200) {
+      var response = this.responseText;
+      document.getElementById('iframe').contentWindow.document.write(response);
+    } else {
+
+    };
   }
+}
