5465af05de092f26ec8685d54d455e32fd2bb7d0
Zachary McGee <zachary.mcgee@siu.edu>
2018-11-10 03:41:19 -0600

    Styling and Navigation

--- js/scrapey.js
+++ js/scrapey.js
@@ -1,3 +1,9 @@
 function loadSite(site){
   document.getElementById('iframe').src = "www." + site;
 }
+
+$("#tasks").click(function(){
+    $.ajax({url: "tasks.html", success: function(result){
+        $("#content").html(result);
+    }});
+});
