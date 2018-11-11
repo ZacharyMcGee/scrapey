e9697c5cff2860d83967994406038a619c9e21c0
Zachary McGee <zachary.mcgee@siu.edu>
2018-11-11 01:56:36 -0600

    New Task Tab Form

--- js/scrapey.js
+++ js/scrapey.js
@@ -11,6 +11,20 @@ for (var i = 0; i < buttons.length; i++) {
   });
 }
 
+function changeTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
 // RELOAD THIS JS FILE AFTER AJAX REQ
 
 function reloadScrapey() {
