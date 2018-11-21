c8857bdfc72be989bb8ee0961400d26f29d8148e
Zachary McGee <zachary.mcgee@siu.edu>
2018-11-21 11:46:47 -0600

    Get Links Python Script

---
+++ python/getLink.py
@@ -0,0 +1,42 @@
+# import libraries
+import sys
+import urllib.request
+from urllib.parse import urljoin
+from bs4 import BeautifulSoup
+
+indexFirst = "first"
+indexAll = "all"
+indexCustom = "custom"
+
+# specify the url
+url = sys.argv[1]
+tag = sys.argv[2]
+selector = sys.argv[3]
+name = sys.argv[4]
+index = sys.argv[5]
+
+# query the website and return the html to the variable ‘page’
+page = urllib.request.urlopen(url)
+
+# parse the html using beautiful soup and store in variable `soup`
+soup = BeautifulSoup(page, 'html.parser')
+
+if index == indexFirst:
+  results = soup.findAll(tag, {selector: name})
+  for tag in results:
+    links = tag.findAll('a')
+    print(links[0])
+elif index == indexAll:
+  results = soup.findAll(tag, {selector: name})
+  for tag in results:
+    links = tag.findAll('a')
+    for a in links:
+        print(a['href'])
+elif index == indexCustom:
+  results = soup.findAll(tag, {selector: name})
+  for tag in results:
+    links = tag.findAll('a')
+    for x in range(indexFrom, indexTo):
+        print(links[x])
+else:
+  results = "error"
