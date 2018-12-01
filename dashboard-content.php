0daf1d43e5e70b3ca5e157cb848e1422f0426521
Zachary McGee <zachary.mcgee@siu.edu>
2018-11-30 23:46:43 -0600

    Update dashboard-content.php

---
+++ dashboard-content.php
@@ -0,0 +1,16 @@
+<div class="full-card">
+  <div class="card-title">
+    <div class="card-title-text">
+      <i class="fas fa-plus-circle" id="tasks"></i><span class="parent-link">Dashboard</span>
+    </div>
+    <div class="card-title-button">
+
+    </div>
+  </div>
+
+  <div class="card-body">
+
+
+
+  </div>
+</div>
